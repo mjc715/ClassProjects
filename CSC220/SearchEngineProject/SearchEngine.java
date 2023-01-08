7ac54d50ee23f14d373228f7a6a2b6d5e4b36a16
Michael Castellucci <91853114+mjc715@users.noreply.github.com>
2023-01-07 21:52:10 -0500

    Create SearchEngine.java

---
+++ CSC220/SearchEngineProject/SearchEngine.java
@@ -0,0 +1,14 @@
+package SearchEngineProject;
+
+import java.util.List;
+
+public interface SearchEngine {
+    /* Collect info from all web pages reachable from URLs in startingURLs. */
+    void collect (Browser browser, List<String> startingURLs);
+
+    /* Search for up to numResults pages containing all searchWords and
+     * return them in an array in order of decreasing importance
+     * (number of references). */
+    String[] search (List<String> searchWords, int numResults);
+}
+
